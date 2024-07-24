b8bd1c6eea35d8e7398577fcd27f05ca8423b75b
Shija-cmd <shijajuma@protonmail.com>
2024-07-24 21:29:30 +0000

    Adding 0-pascal_triangle.py file

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+0-pascal triangle interview project
+"""
+
+
+def pascal_triangle(n):
+    """
+    Returns an empty list if n <= 0
+    """
+    k = []
+    if n <= 0:
+        return k
+    k = [[1]]
+    for i in range(1, n):
+        temp = [1]
+        for j in range(len(k[i - 1]) - 1):
+            curr = k[i - 1]
+            temp.append(k[i - 1][j] + k[i - 1][j + 1])
+        temp.append(1)
+        k.append(temp)
+    return k
