40ff1f5237d212635a58c13f5cac96bbe8e2b86d
Shija-cmd <shijajuma@protonmail.com>
2024-09-12 19:56:13 +0000

    Update

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" rotate the 2d matrix
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """ Rotate an n x n matrix 90 degrees clockwise
+    """
+    for x, y in enumerate(zip(*reversed(matrix))):
+        matrix[x] = list(y)
+
+
+if __name__ == '__main__':
+    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+    """ rotate_2d_matrix(matrix) """
+    rotate_2d_matrix(matrix)
+    print(matrix)
